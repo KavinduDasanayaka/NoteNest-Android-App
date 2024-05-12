7d3857ce7e1758350e823ee0b3829d2aa5f052fa
Kavindu Dasanayaka <Kavindudasanayaka23@gmail.com>
2024-05-12 13:22:33 +0530

    add crud operation, search functionality,Create UI

--- app/src/main/java/com/example/notenestapp/MainActivity.kt
+++ app/src/main/java/com/example/notenestapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.notenestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.notenestapp.database.NoteDatabase
+import com.example.notenestapp.repository.NoteRepository
+import com.example.notenestapp.viewmodel.NodeViewModel
+import com.example.notenestapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var noteViewModel : NodeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+        noteViewModel = ViewModelProvider(this,viewModelProviderFactory)[NodeViewModel::class.java]
     }
 }
